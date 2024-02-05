3109748c0d203c76a66183411bbb7969145281c4
Ravinder2301 <132249859+Ravinder2301@users.noreply.github.com>
2024-02-05 16:35:41 +0530

    Update DeleteBook.jsx

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -13,7 +13,7 @@ const DeleteBook = () => {
 
   const handleDeleteBook = () => {
     setLoading(true);
-    axios.delete(`https://mern-app-backend-ten.vercel.app/books/${id}`)
+    axios.delete(`https://mern-app-backend-rust.vercel.app/books/${id}`)
     .then(() => {
       setLoading(false);
       enqueueSnackbar('Book Deleted Successfully', { variant: 'success'});

782cfdd5d3ded3ec99133ab2cf6d14cbaf49e353
Ravinder2301 <132249859+Ravinder2301@users.noreply.github.com>
2024-02-05 17:13:33 +0530

    Update BookModal.jsx

--- frontend/src/home/BookModal.jsx
+++ frontend/src/home/BookModal.jsx
@@ -5,7 +5,7 @@ import { BiUserCircle } from 'react-icons/bi'
 
 const BookModal = ({ item, onClose }) => {
   return (
-    <div className='fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center'
+    <div className='fixed bg-black bg-opacity-60 p-4 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center'
     onClick={onClose}
     >
       <div
